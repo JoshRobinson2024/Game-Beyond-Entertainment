2c65619471b11bea1922a3f0ab17522cb4de996f
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2025-03-12 15:28:33 +0000

    why does this crash unity, annoyance edition

--- Assets/Scripts/Spiral.cs
+++ Assets/Scripts/Spiral.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class Spiral : MonoBehaviour
 {
     private float angle = 0f;
+    
 
     private Vector2 bulletMoveDirections;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Fire", 0f, 0.1f);
+        InvokeRepeating("FireSpiral", 0f, 0.1f);
     }
-    private void Fire()
-    {
-
-    }
-    // Update is called once per frame
-    void Update()
+    private void FireSpiral()
     {
         
+        angle += 10f;
+        if (angle >= 360)
+        {
+            angle = 0f;
+        }
+        for (int i = 0; 1 <= 1; i++)
+        {
+            float bulDirX = transform.position.x + Mathf.Cos(((angle + 180f * i) * Mathf.PI) / 180f);
+            float bulDirY = transform.position.y + Mathf.Sin(((angle + 180f * i) * Mathf.PI) / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
+            
+        }
+        
     }
+    
 }
