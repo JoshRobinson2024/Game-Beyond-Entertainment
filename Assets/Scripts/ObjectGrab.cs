ea0950f9b6328e33696e7d27c94e6369e8c52e80
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2025-03-25 15:53:52 +0000

    why do they fly off into the distance?!

--- Assets/Scripts/ObjectGrab.cs
+++ Assets/Scripts/ObjectGrab.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectGrab : MonoBehaviour
@@ -11,25 +12,35 @@ public class ObjectGrab : MonoBehaviour
     public GameObject Controller;
     public GameObject Guitar;
     public GameObject Journal;
+    public GameObject player;
+
+    public Rigidbody2D controllerRB;
+    public Rigidbody2D guitarRB;
+    public Rigidbody2D journalRB;
+
+    public Collider2D Controllercol;
+    public Collider2D Guitarcol;
+    public Collider2D Journalcol;
     // Start is called before the first frame update
     void Start()
     {
         EController.SetActive(false);
         EGuitar.SetActive(false);
         EJournal.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (EController.activeInHierarchy&& Input.GetKeyDown(KeyCode.E))
+        if (EController.activeInHierarchy&& Input.GetKeyDown(KeyCode.E)&& grabbedObject!= true)
         {
-            grabbedObject = true;
+            ControllerGrab();
             
         }
         else if(EGuitar.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
         {
-            grabbedObject = true;
+            GuitarGrab();
         }
         else if(EJournal.activeInHierarchy&& Input.GetKeyDown(KeyCode.E))
         {
@@ -64,6 +75,24 @@ public class ObjectGrab : MonoBehaviour
     }
     public void ControllerGrab()
     {
+        EController.SetActive(false);
+        grabbedObject = true;
+        controllerRB.isKinematic = true;
 
+        Controller.transform.parent = player.transform;
+        var newPos = new Vector2(player.transform.position.x, player.transform.position.y +2);
+
+        Controller.transform.position = Vector3.MoveTowards(transform.position, newPos, 1000 * Time.deltaTime);
+    }
+    public void GuitarGrab()
+    {
+        EGuitar.SetActive(false);
+        grabbedObject = true;
+        guitarRB.isKinematic = true;
+
+        Guitar.transform.parent = player.transform;
+        var newPos = new Vector2(player.transform.position.x, player.transform.position.y + 2);
+
+        Guitar.transform.position = Vector3.MoveTowards(transform.position, newPos, 1000 * Time.deltaTime);
     }
 }
