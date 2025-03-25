e72f3459c89d7a43cc20620195b9748005ffe911
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2025-03-25 14:45:38 +0000

    Grab Check

--- Assets/Scripts/ObjectGrab.cs
+++ Assets/Scripts/ObjectGrab.cs
@@ -4,23 +4,66 @@ using UnityEngine;
 
 public class ObjectGrab : MonoBehaviour
 {
-    public GameObject E;
+    public GameObject EController;
+    public GameObject EGuitar;
+    public GameObject EJournal;
+    public bool grabbedObject = false;
+    public GameObject Controller;
+    public GameObject Guitar;
+    public GameObject Journal;
     // Start is called before the first frame update
     void Start()
     {
-        E.SetActive(false);
+        EController.SetActive(false);
+        EGuitar.SetActive(false);
+        EJournal.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (EController.activeInHierarchy&& Input.GetKeyDown(KeyCode.E))
+        {
+            grabbedObject = true;
+            
+        }
+        else if(EGuitar.activeInHierarchy && Input.GetKeyDown(KeyCode.E))
+        {
+            grabbedObject = true;
+        }
+        else if(EJournal.activeInHierarchy&& Input.GetKeyDown(KeyCode.E))
+        {
+            grabbedObject = true;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name.Equals("Player"))
+        if (collision.gameObject.name.Equals("controller"))
         {
-            E.SetActive(true);
+            EController.SetActive(true);
+
         }
+        else if (collision.gameObject.name.Equals("guitar"))
+        {
+            EGuitar.SetActive(true);
+        }
+        else if (collision.gameObject.name.Equals("journal"))
+        {
+            EJournal.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        
+        EController.SetActive(false);
+        EGuitar.SetActive(false);
+        EJournal.SetActive(false);
+        
+    }
+    public void ControllerGrab()
+    {
+
     }
 }
