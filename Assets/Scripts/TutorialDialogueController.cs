5b9698613722a9f15fac218335653c7f8c6cdf20
JoshRobinson2024 <JRobinson2024@elam.co.uk>
2025-05-02 10:26:20 +0100

    (Slightly) better tutorial

---
+++ Assets/Scripts/TutorialDialogueController.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+
+public class TutorialDialogueController : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public SceneManagement manager;
+    public string[] lines;
+    public float textSpeed;
+    public GameObject Will;
+    public AudioSource shadowVoice;
+    public AudioClip darkVoice;
+    public int index;
+    public TMP_Text Text;
+    public GameObject controller;
+    public GameObject Guitar;
+    public GameObject Journal;
+    public string SpawnWillImage;
+    public string DespawnWillImage;
+    public string spawnController;
+    public string spawnObjects;
+    public string dash;
+    public string Pickup;
+    public string Move;
+    public string Throw;
+    public static bool thrown;
+    public static bool moved;
+    public static bool Pickedup;
+    public static bool dashed;
+    public static bool dashCheck;
+    public static bool movedCheck;
+    public static bool thrownCheck;
+    public static bool PickedupCheck;
+    public GameObject arrow;
+    public PlayerMovement mov;
+    
+    private void Start()
+    {
+        mov.locked = true;
+        arrow.SetActive(false);
+        index = 0;
+        Invoke("startDialogue", 0.1f);
+        textComponent.text = string.Empty;
+    }
+    private void Update()
+    {
+        if (textComponent.text == lines[index] && !dashCheck && !PickedupCheck && !movedCheck && !thrownCheck)
+        {
+            arrow.SetActive(true);
+            
+        }
+        if (mov.locked)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (textComponent.text == lines[index])
+                {
+                    NextLine();
+                    arrow.SetActive(false);
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    textComponent.text = lines[index];
+                    
+                }
+            }
+        }
+        if (dashed && dashCheck)
+        {
+            dashCheck = false;
+            dashed = false;
+            Invoke("NextLine", 2);
+        }
+        if (thrownCheck && thrown)
+        {
+            thrownCheck = false; 
+            thrown = false;
+            Invoke("NextLine", 2);
+        }
+        if (moved && movedCheck)
+        {
+            moved = false;
+            movedCheck = false;
+            Invoke("NextLine", 2);
+        }
+        if(Pickedup && PickedupCheck)
+        {
+            Pickedup = false;
+            PickedupCheck = false;
+            Invoke("NextLine", 2);
+        }
+    }
+    public void startDialogue()
+    {
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        if (lines[index] == spawnController)
+        {
+            controller.SetActive(true);
+        }
+        if(lines[index] == spawnObjects)
+        {
+            Guitar.SetActive(true);
+            Journal.SetActive(true);
+        }
+        if (lines[index] == SpawnWillImage)
+        {
+            Will.SetActive(true);
+        }
+        if (lines[index] == DespawnWillImage)
+        {
+            Will.SetActive(false);
+        }
+        if (lines[index] == dash)
+        {
+            dashCheck = true;
+            mov.locked = false;
+        }
+        if (lines[index] == Pickup)
+        {
+            PickedupCheck = true;
+            mov.locked = false;
+        }
+        if (lines[index] == Move)
+        {
+            movedCheck = true;
+            mov.locked = false;
+        }
+        if (lines[index] == Throw)
+        {
+            thrownCheck = true;
+            mov.locked = false;
+        }
+        shadowVoice.PlayOneShot(darkVoice);
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+            
+
+        }
+    }
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            mov.locked = true;
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            manager.loadInteractionSelect();
+
+        }
+    }
+}    
+
+
+
